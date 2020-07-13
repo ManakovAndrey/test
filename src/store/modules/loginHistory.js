327a446435abdb2172c42d2b204fd7e7eed45362
andrew.manakov <andrew.manakov@buidl.ru>
2020-07-13 19:17:29 +0300

    completed tasks

--- src/store/modules/loginHistory.js
+++ src/store/modules/loginHistory.js
@@ -1,17 +1,26 @@
 const state = {
-  error: 'kkeeeek',
+  loginAttempts: [],
 };
 
 const getters = {
 };
 
 const mutations = {
-  SET_OFFER_DATA(state, data) {
-    state.offerData = data;
+  SET_LOGIN_ATTEMPT(state, type) {
+    state.loginAttempts.push({ text: `Попытка авторизации: ${type === 'success' ? 'Успех' : 'Неудача'}; Дата: ${new Date()}`, type });
+  },
+  CLEAR_LOGIN_ATTEMPTS(state) {
+    state.loginAttempts = [];
   },
 };
 
 const actions = {
+  addLoginAttempt({commit}, payload) {
+    commit('SET_LOGIN_ATTEMPT', payload);
+  },
+  clearLoginAttempts({commit}) {
+    commit('CLEAR_LOGIN_ATTEMPTS');
+  },
 };
 
 
--- src/store/modules/main.js
+++ src/store/modules/main.js
@@ -1,3 +1,6 @@
+// import mainApi from '@/api/mainApi';
+import { tryLogin } from '@/api/mockRequests';
+
 const state = {
   isLogin: false,
 };
@@ -6,12 +9,34 @@ const getters = {
 };
 
 const mutations = {
-  SET_OFFER_DATA(state, data) {
-    state.offerData = data;
+  SET_LOGIN_STATE(state, data) {
+    state.isLogin = data;
   },
 };
 
 const actions = {
+  // eslint-disable-next-line
+  async tryLogin({commit, dispatch}, payload) {
+    // try {
+    //   await mainApi.post('logIn', {
+    //     login: payload.login,
+    //     password: payload.password,
+    //   });
+    // } catch (e) {
+    //   console.log('error: ', e);
+    // }
+    try {
+      await tryLogin( payload.login, payload.password);
+      dispatch('loginHistory/addLoginAttempt', 'success', { root: true })
+      commit('SET_LOGIN_STATE', true)
+    } catch (e) {
+      dispatch('loginHistory/addLoginAttempt', 'error', { root: true })
+      throw 'error';
+    }
+  },
+  logout({ commit }) {
+    commit('SET_LOGIN_STATE', false);
+  },
 };
 
 
